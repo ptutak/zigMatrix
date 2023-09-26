1081bdccf6bb08081fdd213af55724afbe2b860b
Piotr Tutak <piotr.tutak@getindata.com>
2023-09-26 11:38:52 +0200

    physics

--- src/zigPhysics/solid.zig
+++ src/zigPhysics/solid.zig
@@ -1,22 +1,46 @@
 const std = @import("std");
 
+const G: f64 = 6.67408e-11;
+
+const Force = struct {
+    x: f64,
+    y: f64,
+};
+
+const ForceField = struct {
+    Fx: f64,
+    Fy: f64,
+
+    pub fn get_force(self: *const ForceField, point: *const Point) Force {
+        _ = point;
+        return Force{ .x = self.Fx, .y = self.Fy };
+    }
+};
+
+const Field = union {
+    force_field: ForceField,
+};
+
 const Point = struct {
     x: f64,
     y: f64,
     vx: f64,
     vy: f64,
     m: f64,
-    Fx: f64,
-    Fy: f64,
 
-    pub fn new(x: f64, y: f64, m: f64) Point {
-        return Point{
-            .x = x,
-            .y = y,
-            .m = m,
-            .Fx = 0.0,
-            .Fy = 0.0,
-        };
+    pub fn calculate_force(self: *const Point, fields: []Field) Force {
+        var Fx: f64 = 0.0;
+        var Fy: f64 = 0.0;
+        for (fields) |field| {
+            switch (field) {
+                .force_field => {
+                    const force = field.force_field.get_force(self);
+                    Fx += force.x;
+                    Fy += force.y;
+                },
+            }
+        }
+        return Force{ .x = Fx, .y = Fy };
     }
 
     pub fn update(self: *Point, dt: f64) void {

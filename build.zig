95f5441f3c5e8b5004c03c58ab3c7a4a4e0e3b8c
Piotr Tutak <p.tutak@outlook.com>
2023-10-05 01:03:04 +0200

    discover modules

--- build.zig
+++ build.zig
@@ -1,20 +1,41 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn discover_modules(b: *std.Build, root_source_path: []const u8) !std.ArrayList(*std.Build.Module) {
+    const dir_iterator = try std.fs.cwd().openIterableDir(root_source_path, .{});
+    var iterator = dir_iterator.iterate();
+    var array = std.ArrayList(*std.Build.Module).init(std.heap.page_allocator);
+
+    while (try iterator.next()) |path| {
+        switch (path.kind) {
+            .directory => {
+                std.debug.print("directory: {s}\n", .{path.name});
+                const subdir = try std.mem.join(std.heap.page_allocator, "/", &[_][]const u8{ root_source_path, path.name });
+                const modules = try discover_modules(b, subdir);
+                for (modules.items) |module| {
+                    try array.append(module);
+                }
+            },
+            .file => {
+                if (std.mem.eql(u8, path.name[path.name.len - 4 .. path.name.len], ".zig")) {
+                    const module_path = try std.mem.join(std.heap.page_allocator, "/", &[_][]const u8{ root_source_path, path.name });
+                    const module_name: []const u8 = module_path[0 .. module_path.len - 4];
+                    var output_module_name: []u8 = try std.heap.page_allocator.alloc(u8, module_name.len);
+                    _ = std.mem.replace(u8, module_name, "/", ".", output_module_name);
+                    const module = b.addModule(output_module_name, .{ .source_file = .{ .path = module_path } });
+                    try array.append(module);
+                }
+            },
+            else => {},
+        }
+    }
+    return array;
+}
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-
+    const modules = try discover_modules(b, "src");
     const common = b.addModule("common", .{ .source_file = .{ .path = "src/common.zig" } });
     const zigPhysics_points = b.addModule("zigPhysics.points", .{ .source_file = .{ .path = "src/zigPhysics/points.zig" } });
     const unit_tests = b.addTest(.{
@@ -22,14 +43,13 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
+    for (modules.items) |module| {
+        std.debug.print("module: {any}\n", .{module});
+    }
     unit_tests.addModule("common", common);
     unit_tests.addModule("zigPhysics.points", zigPhysics_points);
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
